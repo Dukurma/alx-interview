709b1502044dc51e7203fc34183ef98b229d20f9
Dukurma <barryyabilsu@gmail.com>
2023-10-03 06:55:41 +0000

    lock box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+'''A module for working with lockboxes.
+'''
 
-from collection import deque
 
 def canUnlockAll(boxes):
-    num_boxes = len(boxes)  # Total number of boxes
-    unlocked = [False] * num_boxes # Track if each box is unlocked
-    unlocked[0] = True  # The first box is initially unlock
-    queue = deque([0])  # Start BFS from the first box
-    
-    while queue:
-        current_box = queue.popleft()
-        for key in boxes[current_box]:
-            if key < num_boxes and not unlocked[key]:
-                unlocked[key] = True
-                queue.append(key)
-                
-    return all(unlocked)
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
